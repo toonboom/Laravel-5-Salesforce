c8b3d46ab5e5c95813338dd246281678c8bf2374
jharfouche <jharfouche@toonboom.com>
2024-01-05 15:43:38 -0500

    dep upgrades+mv some env values to config to allow config caching

--- src/Davispeixoto/Laravel5Salesforce/config/config.php
+++ src/Davispeixoto/Laravel5Salesforce/config/config.php
@@ -13,5 +13,13 @@ return [
     'username' => env('SALESFORCE_USERNAME'),
     'password' => env('SALESFORCE_PASSWORD'),
     'token' => env('SALESFORCE_TOKEN'),
-    'wsdl' => storage_path('app/' . env('SALESFORCE_WSDL', 'wsdl/enterprise.wsdl.xml'))
+    'wsdl' => storage_path('app/' . env('SALESFORCE_WSDL', 'wsdl/enterprise.wsdl.xml')),
+
+    'sf_api_client_id' => env('SF_API_CLIENT_ID'),
+    'sf_api_client_secret' => env('SF_API_CLIENT_SECRET'),
+    'sf_api_endpoint' => env('SF_API_ENDPOINT'),
+    'sf_api_login_endpoint' => env('SF_API_LOGIN_ENDPOINT'),
+    'sf_api_password' => env('SF_API_PASSWORD'),
+    'sf_api_security_token' => env('SF_API_SECURITY_TOKEN'),
+    'sf_api_username' => env('SF_API_USERNAME')
 ];
